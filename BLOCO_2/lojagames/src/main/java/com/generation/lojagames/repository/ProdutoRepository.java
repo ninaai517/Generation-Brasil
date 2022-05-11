c7a9c28d6096741a200a45076c277ef4722b5144
Carolina Aizawa Moreira <102269777+ninaai517@users.noreply.github.com>
2022-05-11 20:52:09 -0300

    Criando API Loja de Games

---
+++ BLOCO_2/lojagames/src/main/java/com/generation/lojagames/repository/ProdutoRepository.java
@@ -0,0 +1,20 @@
+package com.generation.lojagames.repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.lojagames.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+	
+	public List<Produto> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
+	
+	public List <Produto> findByPrecoGreaterThanOrderByPreco(BigDecimal preco);
+	
+	public List <Produto> findByPrecoLessThanOrderByPrecoDesc(BigDecimal preco);
+}
+
+
